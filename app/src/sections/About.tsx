c6f6aeb05436ee7cf1a332e3dbb6887c20cb7375
paganoingvincenzo <paganoingvincenzo@gmail.com>
2026-02-06 11:14:05 +0100

    Update About.tsx

--- app/src/sections/About.tsx
+++ app/src/sections/About.tsx
@@ -5,7 +5,7 @@ const engineers = [
   {
     name: 'Ing. Giovanni Costanzo',
     role: 'Co-fondatore',
-    image: '/team/Giovanni Costanzo.jpg',
+    image: '/team/Giovanni-Costanzo.jpg',
     albo: 'N. 3919 - Sez. A Albo Ingegneri Caserta',
     ctu: 'Consulente Tecnico d\'Ufficio - Tribunale Napoli Nord',
     specialization: [
@@ -22,7 +22,7 @@ const engineers = [
   {
     name: 'Ing. Vincenzo Pagano',
     role: 'Co-fondatore',
-    image: '/team/Vincenzo Pagano.jpg',
+    image: '/team/Vincenzo-Pagano.jpg',
     albo: 'Sez. A Albo Ingegneri di Caserta',
     ctu: 'Esperto in Gestione dell\'Energia (E.G.E.) certificato',
     specialization: [
