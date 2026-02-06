b0f6bb03d169a3581d9fb648849451da493be4a9
paganoingvincenzo <paganoingvincenzo@gmail.com>
2026-02-06 15:46:34 +0100

    Update Navbar.tsx

--- app/src/sections/Navbar.tsx
+++ app/src/sections/Navbar.tsx
@@ -57,13 +57,13 @@ export default function Navbar() {
         }`}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-20">
+          <div className="flex items-center justify-between h-auto py-4">
             {/* Logo */}
             <div className="flex items-center gap-2">
               <img
    src={logoImg}
    alt="Logo Studio Costanzo-Pagano"
-   className="w-auto h-16 rounded-lg object-contain" // 'object-contain' evita che il logo venga tagliato
+   className="w-auto h-32 md:h-40 rounded-lg object-contain" // 'object-contain' evita che il logo venga tagliato
 />
 
               <div className="hidden sm:block">
