9240d7a4c4c351f80251754937815cb891846988
paganoingvincenzo <paganoingvincenzo@gmail.com>
2026-02-14 13:04:07 +0100

    Delete app/not-found.tsx

--- app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <main style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Pagina non trovata</h1>
-      <p>L’indirizzo richiesto non esiste.</p>
-    </main>
-  );
-}
-
